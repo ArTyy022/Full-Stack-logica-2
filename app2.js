558c7791cd7d6ca2d576a7af273c4666fcaeb644
ArTyy022 <toledoarthur11@gmail.com>
2025-07-18 15:02:51 -0300

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,53 @@
+//Desafio 1
+/*
+function exibirOla() {
+  console.log("Olá, mundo!");
+}
+
+exibirOla(); */
+
+
+//Desafio 2
+/*
+function exibirOlaNome(nome) {
+  console.log(`Olá, ${nome}!`);
+}
+
+exibirOlaNome("Arthur"); */
+
+
+//Desafio 3
+/*
+function calcularDobro(numero) {
+  return numero * 2;
+}
+
+let resultadoDobro = calcularDobro(5);
+console.log(resultadoDobro); */
+
+//Desafio 4
+/*
+function calcularMedia(a, b, c) {
+  return (a + b + c) / 3;
+}
+
+let media = calcularMedia(4, 7, 10);
+console.log(media); */
+
+//Desafio 5
+/*
+function encontrarMaior(a, b) {
+  return a > b ? a : b;
+}
+
+let maiorNumero = encontrarMaior(15, 9);
+console.log(maiorNumero); */
+
+//Desafio 6
+/*
+function quadrado(numero) {
+  return numero * numero;
+}
+
+let resultado = quadrado(2);
+console.log(resultado); */
