d06d3992de57cd032a14534e73c3e2760ccea136
ArTyy022 <toledoarthur11@gmail.com>
2025-07-19 23:12:25 -0300

    Add files via upload

---
+++ app4.js
@@ -0,0 +1,28 @@
+//Desafio 1
+let listaGenerica = [];
+
+//Desafio 2
+/*
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+*/
+
+//Desafio 3
+/*
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang'); */
+
+//Desafio 4
+/*
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]); */
+
+
+//Desafio 5
+/*
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]); */
+
+//Desafio 6
+
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
